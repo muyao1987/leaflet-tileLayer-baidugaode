da37157fa398eb84643661c6690ba6f2ea2a06d3
wanghao <muyao1987@qq.com>
2017-08-01 16:05:32 +0800

    初始化

---
+++ src/tileLayer.baidu.js
@@ -0,0 +1,53 @@
+﻿//百度地图底图调用插件，木遥原创（QQ：346819890）
+
+//请引入 proj4.js 和 proj4leaflet.js
+L.CRS.Baidu = new L.Proj.CRS('EPSG:900913', '+proj=merc +a=6378206 +b=6356584.314245179 +lat_ts=0.0 +lon_0=0.0 +x_0=0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
+    resolutions: function () {
+        level = 19
+        var res = [];
+        res[0] = Math.pow(2, 18);
+        for (var i = 1; i < level; i++) {
+            res[i] = Math.pow(2, (18 - i))
+        }
+        return res;
+    }(),
+    origin: [0, 0],
+    bounds: L.bounds([20037508.342789244, 0], [0, 20037508.342789244])
+});
+
+L.tileLayer.baidu = function (option) {
+    option = option || {};
+
+    var layer;
+    var subdomains = '012';
+    switch (option.layer) {
+        case "vec":
+        default: 
+            //'http://online{s}.map.bdimg.com/tile/?qt=tile&x={x}&y={y}&z={z}&styles=pl&b=0&limit=60&scaler=1&udt=20170525'
+            layer = L.tileLayer('http://online{s}.map.bdimg.com/onlinelabel/?qt=tile&x={x}&y={y}&z={z}&styles=' + (option.bigfont ? 'ph' : 'pl') + '&udt=20170525&scaler=1', {
+                subdomains: subdomains,
+                tms: true
+            });
+            break;
+        case "img":
+            layer = L.layerGroup([
+				L.tileLayer('http://shangetu{s}.map.bdimg.com/it/u=x={x};y={y};z={z};v=009;type=sate&fm=46&udt=20170525', {
+				    subdomains: subdomains,
+				    tms: true
+				}),
+				L.tileLayer('http://online{s}.map.bdimg.com/tile/?qt=tile&x={x}&y={y}&z={z}&styles=' + (option.bigfont ? 'sh' : 'sl') + '&v=017&udt=20170525', {
+				    subdomains: subdomains,
+				    tms: true
+				})
+            ]);
+            break;
+        case "time"://实时路况
+            var time = new Date().getTime();
+            layer = L.tileLayer('http://its.map.baidu.com:8002/traffic/TrafficTileService?x={x}&y={y}&level={z}&time=' + time + '&label=web2D&v=017', {
+                subdomains: subdomains,
+                tms: true
+            });
+            break;
+    }
+    return layer;
+};
